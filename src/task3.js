a3f62ece8514c1418d3576e0b255387867f4422d
miguel angel <miguelangelcamposhipolito@gmail.com>
2026-09-01 13:11:44 -0600

    resolvi mi error task3

--- src/task3.js
+++ src/task3.js
@@ -2,9 +2,8 @@ const arr = [1,2,3,4,5,6,7,9,10,11,12]
 
 // Escribe tu codigo por debajo de esta linea
 
-arr.reverse();//de nuermo los revertimos
-arr.splice(12, 0, 13, 14, 15, 16, 17, 18, 19, 20);//coloque asta el 20 ya que asi an sido los ejercicios
-
+arr.splice(7, 0, 8,);//me posiscione en el indice 7 borre 0 y coloque el 8
+console.log(arr)
 // TEscribe tu codigo por encima de esta linea
 
 //No tocar esta funcion
